7be83f67b76082e17e0fea92b92b3695ce66dba4
Vasiliy Lifanovskiy <vasiliy.lifanovskiy@ddgcorp.ru>
2017-08-18 16:40:23 +0300

    try show output hello.py with \n

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
 def app(environ, start_response):
-	q_string = environ.get('QUERY_STRING')
-	pairs_list = q_string.split("&")
-	body = ""
-	for pair in pairs_list:
-		body += pair + "\n"
+
+	data = environ.get('QUERY_STRING')
+	m = [bytes(i+'\n','ascii') for i in data.split('&')]
+
+	#q_string = environ.get('QUERY_STRING')
+	#pairs_list = q_string.split("&")
+	#body = ""
+	#for pair in pairs_list:
+	#	body += pair + "\n"
 
 	response_string = [('Content-type', 'text/plain')]
 
 	start_response('200 OK', response_string)
-	return [body.strip().encode()]
-	
+	#return [body.strip().encode()]
+	return [m]
